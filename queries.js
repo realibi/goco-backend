c352b1f5ceebdc68e346b0f863edec44e12bc98f
Duysenaliyev Alibi <40587377+realibi@users.noreply.github.com>
2021-08-05 12:06:44 +0600

    Update queries.js

--- queries.js
+++ queries.js
@@ -1417,54 +1417,55 @@ const updateCallCenterRow = (request, response) => {
         firstCall,
         firstCallTime,
         secondCallTime,
-        secondCall
+        secondCall,
+        categoryId
     } = request.body;
 
-    if(meetingDate !== null && meetingTime !== null){
-        pool.query('UPDATE public.crm SET center_name=$2, contact_name=$4, center_phone=$3, center_email=$5, first_call_date=$11, first_call_time=$12, first_call_comment=$7, kp_send_date=$6, second_call_date=$14, second_call_time=$13, second_call_comment=$8, meeting_date=$9, meeting_time=$10 WHERE id=$1',
-            [
-                id,
-                companyName,
-                phone,
-                contactPerson,
-                mail,
-                sendKPDate,
-                firstCallComment,
-                secondCallComment,
-                meetingDate === "" ? null : meetingDate,
-                meetingTime === "" ? null : meetingTime,
-                firstCall,
-                firstCallTime,
-                secondCallTime,
-                secondCall
-            ], (error, results) => {
-                if (error) {
-                    throw error
-                }
+    if (meetingDate !== null && meetingTime !== null) {
+        pool.query('UPDATE public.crm SET center_name=$2, center_category_id=$15, contact_name=$4, center_phone=$3, center_email=$5, first_call_date=$11, first_call_time=$12, first_call_comment=$7, kp_send_date=$6, second_call_date=$14, second_call_time=$13, second_call_comment=$8, meeting_date=$9, meeting_time=$10 WHERE id=$1', [
+            id,
+            companyName,
+            phone,
+            contactPerson,
+            mail,
+            sendKPDate,
+            firstCallComment,
+            secondCallComment,
+            meetingDate === "" ? null : meetingDate,
+            meetingTime === "" ? null : meetingTime,
+            firstCall,
+            firstCallTime,
+            secondCallTime,
+            secondCall,
+            categoryId
+        ], (error, results) => {
+            if (error) {
+                throw error
+            }
 
-                response.status(200).json(true);
+            response.status(200).json(true);
         })
-    }else{
-        pool.query('UPDATE public.crm SET center_name=$2, contact_name=$4, center_phone=$3, center_email=$5, first_call_date=$9, first_call_time=$10, first_call_comment=$7, kp_send_date=$6, second_call_date=$12, second_call_time=$11, second_call_comment=$8 WHERE id=$1',
-            [
-                id,
-                companyName,
-                phone,
-                contactPerson,
-                mail,
-                sendKPDate,
-                firstCallComment,
-                secondCallComment,
-                firstCall,
-                firstCallTime,
-                secondCallTime,
-                secondCall
-            ], (error, results) => {
-                if (error) {
-                    throw error
-                }
+    } else {
+        pool.query('UPDATE public.crm SET center_name=$2, center_category_id=$13, contact_name=$4, center_phone=$3, center_email=$5, first_call_date=$9, first_call_time=$10, first_call_comment=$7, kp_send_date=$6, second_call_date=$12, second_call_time=$11, second_call_comment=$8 WHERE id=$1', [
+            id,
+            companyName,
+            phone,
+            contactPerson,
+            mail,
+            sendKPDate,
+            firstCallComment,
+            secondCallComment,
+            firstCall,
+            firstCallTime,
+            secondCallTime,
+            secondCall,
+            categoryId
+        ], (error, results) => {
+            if (error) {
+                throw error
+            }
 
-                response.status(200).json(true);
+            response.status(200).json(true);
         })
     }
 }
