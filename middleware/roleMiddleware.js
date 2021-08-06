acc8f214eb3101b4cd7c1a968eeacd0fe0bdeb0e
realibi <reallibi@gmail.com>
2021-08-06 12:04:17 +0600

    crm draft

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -13,7 +13,8 @@ export default function roleMiddleware(roles) {
             console.log(req.headers.authorization)
             const token = req.headers.authorization.split(' ')[1]
             if (!token) {
-                return res.status(403).json({message: "Пользователь не авторизован"})
+                // return res.status(403).json({message: "Пользователь не авторизован"})
+                res.redirect("/login");
             }
             const decodedData = jwt.verify(token, secret)
 
