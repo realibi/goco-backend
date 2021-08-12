0800b0676555e46cf7b39e8b553b38fcff7a27df
realibi <reallibi@gmail.com>
2021-08-12 16:49:17 +0600

    call-center draft

--- queries.js
+++ queries.js
@@ -1476,7 +1476,7 @@ const updateCallCenterRow = (request, response) => {
         })
     } else {
         console.log("update без указания встречи");
-        let query = `UPDATE public.crm SET center_name='${companyName}', center_category_id=${categoryId}, contact_name='${contactPerson}', center_phone='${phone}', center_email='${mail}', first_call_date=${firstCall === null ? null : `'${firstCall}'`}, first_call_time=${firstCallTime === null ? null : `'${firstCallTime}'`}, first_call_comment='${firstCallComment}', kp_send_date='${sendKPDate}', second_call_date=${secondCall === null ? null : `'${secondCall}'`}, second_call_time=${secondCallTime === null ? null : `'${secondCallTime}'`}, second_call_comment='${secondCallComment}' WHERE id=${id}`;
+        let query = `UPDATE public.crm SET center_name='${companyName}', center_category_id=${categoryId}, contact_name='${contactPerson}', center_phone='${phone}', center_email='${mail}', first_call_date=${firstCall === null ? null : `'${firstCall}'`}, first_call_time=${firstCallTime === null ? null : `'${firstCallTime}'`}, first_call_comment='${firstCallComment}', kp_send_date=${sendKPDate === null ? null : `'${sendKPDate}'`}, second_call_date=${secondCall === null ? null : `'${secondCall}'`}, second_call_time=${secondCallTime === null ? null : `'${secondCallTime}'`}, second_call_comment='${secondCallComment}' WHERE id=${id}`;
         console.log(query)
         pool.query(query, (error, results) => {
             if (error) {
