65783e4a632ad39178574c71313f0f53f7a0b0a1
realibi <reallibi@gmail.com>
2021-08-31 09:56:04 +0600

    redirect if user not authorized

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,6 +9,7 @@ export default function authMiddleware (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
+            res.redirect("/login");
             return res.status(403).json({message: "Пользователь не авторизован"})
         }
         req.user = jwt.verify(token, secret)
