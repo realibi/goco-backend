73c5159869132a3867856c438320023c888f3957
realibi <reallibi@gmail.com>
2021-09-09 19:13:12 +0600

    cards order changing draft

--- queries.js
+++ queries.js
@@ -1125,30 +1125,30 @@ const logUserClick = (request, response) => {
         if (error) {
             throw error
         }
-
+        response.status(201).send('ok')
     })
 
-    pool.query(`select views from subcourses where id=${courseId}`, (error, result) => {
-        if (error) {
-            throw error
-        }
-        let viewsCount = result.rows[0].views;
-        if((viewsCount + 1) % 3 === 0){
-            pool.query(`select max(order_coefficient) as "max" from subcourses`, (error, orderResult) => {
-                if (error) {
-                    throw error
-                }
-                let maxOrderCoefficient = orderResult.rows[0].max;
-
-                pool.query(`update subcourses set views=${viewsCount+1}, order_coefficient=${maxOrderCoefficient+0.1}`, (error, updateResult) => {
-                    if (error) {
-                        throw error
-                    }
-                    response.status(200).json({event: `Click log: ${eventName}`, order_coefficient: maxOrderCoefficient+0.1});
-                })
-            })
-        }
-    })
+    // pool.query(`select views from subcourses where id=${courseId}`, (error, result) => {
+    //     if (error) {
+    //         throw error
+    //     }
+    //     let viewsCount = result.rows[0].views;
+    //     if((viewsCount + 1) % 3 === 0){
+    //         pool.query(`select max(order_coefficient) as "max" from subcourses`, (error, orderResult) => {
+    //             if (error) {
+    //                 throw error
+    //             }
+    //             let maxOrderCoefficient = orderResult.rows[0].max;
+    //
+    //             pool.query(`update subcourses set views=${viewsCount+1}, order_coefficient=${maxOrderCoefficient+0.1}`, (error, updateResult) => {
+    //                 if (error) {
+    //                     throw error
+    //                 }
+    //                 response.status(200).json({event: `Click log: ${eventName}`, order_coefficient: maxOrderCoefficient+0.1});
+    //             })
+    //         })
+    //     }
+    // })
 }
 
 //----------------------------------------------------------
