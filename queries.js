1bda4ec412de4a8a3874a37c2e836e51ce7d6bfa
realibi <reallibi@gmail.com>
2021-09-25 01:35:16 +0600

    application responded cards fetching draft

--- queries.js
+++ queries.js
@@ -2341,17 +2341,18 @@ const getApplicationResponses = (request, response) => {
         if(cardsIds.length > 0){
             let cardsIdsString = cardsIds.join(',');
             cardsFetchQuery = `SELECT subcourses.id, subcourses.isonline, subcourses.title, courses.website_url, subcourses.currency, subcourses.unit_of_time, subcourses.description, subcourses.ages, subcourses.type, subcourses.format, subcourses.price, subcourses.schedule, subcourses.expected_result, subcourses.start_requirements, subcourses.duration, subcourses.rating, courses.id as "course_id", courses.title as "course_title", courses.phones, courses.instagram, courses.latitude, courses.longitude, courses.url, courses.img_src, courses.background_image_url from subcourses inner join courses on subcourses.course_id = courses.id where subcourses.id in (${cardsIdsString}) and subcourses.approved=true and subcourses.is_archived=false order by order_coefficient asc`;
+            pool.query(cardsFetchQuery, (error, cardsResults) => {
+                if (error) {
+                    throw error
+                }
+                console.log(cardsResults.rows)
+                response.status(200).json(cardsResults.rows);
+            })
         }else{
-            cardsFetchQuery = `SELECT subcourses.id, subcourses.isonline, subcourses.title, courses.website_url, subcourses.currency, subcourses.unit_of_time, subcourses.description, subcourses.ages, subcourses.type, subcourses.format, subcourses.price, subcourses.schedule, subcourses.expected_result, subcourses.start_requirements, subcourses.duration, subcourses.rating, courses.id as "course_id", courses.title as "course_title", courses.phones, courses.instagram, courses.latitude, courses.longitude, courses.url, courses.img_src, courses.background_image_url from subcourses inner join courses on subcourses.course_id = courses.id where subcourses.approved=true and subcourses.is_archived=false order by order_coefficient asc`;
+            response.status(200).json([]);
         }
 
-        pool.query(cardsFetchQuery, (error, cardsResults) => {
-            if (error) {
-                throw error
-            }
-            console.log(cardsResults.rows)
-            response.status(200).json(cardsResults.rows);
-        })
+
     })
 }
 
